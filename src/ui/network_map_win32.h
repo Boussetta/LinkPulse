713151148e6478917cfe2854e50efa9a64888ab6
Wissem Boussetta <wissem.boussetta@outlook.com>
2026-09-08 20:47:16 +0200

    feat: add network map flyout

---
+++ src/ui/network_map_win32.h
@@ -0,0 +1,12 @@
+#ifndef LINKPULSE_NETWORK_MAP_WIN32_H
+#define LINKPULSE_NETWORK_MAP_WIN32_H
+
+#include "linkpulse/discovery.h"
+
+#include <windows.h>
+
+HWND lp_network_map_create(HINSTANCE instance, HWND owner);
+void lp_network_map_show(HWND window, const lp_neighbor_list_t *neighbors,
+                         const lp_local_network_list_t *networks);
+
+#endif /* LINKPULSE_NETWORK_MAP_WIN32_H */
